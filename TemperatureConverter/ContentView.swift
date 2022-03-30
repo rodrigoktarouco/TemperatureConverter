2a28f2df9f78b2f0d67255b07e42480d90fc324c
Rodrigo Kroef tarouco <55713305+rodrigoktarouco@users.noreply.github.com>
2022-03-30 10:45:43 -0300

    Initial Commit

---
+++ TemperatureConverter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TemperatureConverter
+//
+//  Created by Rodrigo Tarouco on 30/03/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TemperatureConverter/TemperatureConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TemperatureConverterApp.swift
+//  TemperatureConverter
+//
+//  Created by Rodrigo Tarouco on 30/03/22.
+//
+
+import SwiftUI
+
+@main
+struct TemperatureConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
